50122c3c45e39207c0c8835c51c69f54af5a9a34
Tom Stephenson <thomas.a.stephenson@gmail.com>
2019-01-14 00:35:57 +1300

    Revert "Updated captions"
    This reverts commit 5b95ccc24f10c6a39b309830472557c7007531f6.

--- Assignment 5/js/serviceworker.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
-  .then(function() {
-    console.log('Service worker registration successful!');
-  })
-  .catch(function() {
-    console.log('Service worker registration failed');
-  });
-}
