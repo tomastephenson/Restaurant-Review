dbec4c79b3ca90f9377d168961086e6fd07c3a6d
Tom Stephenson <thomas.a.stephenson@gmail.com>
2019-01-17 01:07:59 +1300

    Fixed caching code
    Using the page https://developers.google.com/web/fundamentals/primers/service-workers/ as a reference to recreate my caching code, and actually testing using Chrome's developer tools to run the site like it was offline, rather than unknowingly utilising Chrome's built-in caching and thinking my caching code was working!!
    
    The caching code now appears to be working across multiple pages when the network throttling is set to offline.

--- js/serviceworker.js
+++ js/serviceworker.js
@@ -1,9 +1,12 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
-  .then(function() {
-    console.log('Service worker registration successful!');
-  })
-  .catch(function() {
-    console.log('Service worker registration failed');
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // Registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
   });
 }
+
--- sw.js
+++ sw.js
@@ -1,63 +1,74 @@
-let cacheName = 'restaurant-content';
+let CACHE_NAME = 'restaurant-content';
 
 // Create an array of the URLs we want to cache.
-let cachedUrls = [
-        '/',
-        '/index.html',
-        '/restaurant.html',
-        'css/styles.css',
-        'data/restaurants.json',
-        'img/1.jpg',
-        'img/2.jpg',
-        'img/3.jpg',
-        'img/4.jpg',
-        'img/5.jpg',
-        'img/6.jpg',
-        'img/7.jpg',
-        'img/8.jpg',
-        'img/9.jpg',
-        'img/10.jpg',
-        'js/dbhelper.js',
-        'js/main.js',
-        'js/restaurant_info.js',
+let urlsToCache = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    'css/styles.css',
+    'data/restaurants.json',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg',
+    'js/dbhelper.js',
+    'js/main.js',
+    'js/restaurant_info.js',
+];
 
-        // Install the service worker
+// Install the service worker
 
-        self.addEventListener('install', function(event) {
-            //
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
 
-            event.waitUntil(
 
-                // Utilise the cache interface so that we can store all items we want to cache
 
-                caches.open(cacheName).then(function(cache) {
 
-                    // Then add all the cached URLs from the previosuly-defined array.
+// Listens for the activate event that signifies that the service worker has been installed
 
-                    return cache.addAll(cachedUrls);
-                })
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+
+            return fetch(event.request).then(
+                function(response) {
+                    // Check if we received a valid response
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    // Utilising the cloning portion of the code from the Google developers page so that we can cache new requests cumulatively
+                    // The previous block of code worked if the page depth = 1. This allows the service worker to cache content as we navigate through multiple pages.
+                    // Reference material from here: https://developers.google.com/web/fundamentals/primers/service-workers/
+                    var responseToCache = response.clone();
+
+                    caches.open(CACHE_NAME)
+                        .then(function(cache) {
+                            cache.put(event.request, responseToCache);
+                        });
+
+                    return response;
+                }
             );
-        });
-
-
-        // Listens for the activate event that signifies that the service worker has been installed
-
-        self.addEventListener('activate', function(event) {
-            event.waitUntil(
-
-                // Returns a Promise that resolves to an array of all Cache keys.
-
-                caches.keys().then(function(keys) {
-                    return Promise.all(
-
-                        // Filters out any cache keys that aren't in our previously-defined array list and deletes them.
-                        keys.filter(function(key) {
-                            return key.startsWith('restaurant-content') &&
-                                key != cacheName;
-                        }).map(function(key) {
-                            return caches.delete(key);
-                        })
-                    );
-                })
-            );
-        });
+        })
+    );
+});
